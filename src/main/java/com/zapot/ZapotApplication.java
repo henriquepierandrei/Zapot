d81e9b4f9e9030fbdfc68c112130a38e541d915c
henriquepierandrei <henriquepierandrei1@gmail.com>
2026-01-03 20:22:15 -0300

    first commit

---
+++ src/main/java/com/zapot/ZapotApplication.java
@@ -0,0 +1,13 @@
+package com.zapot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zapot/ZapotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
